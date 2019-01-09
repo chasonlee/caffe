7ae7bf0385c8aa104b2ae07ea3e2551364c9bdc1
chasonlee <329546652@qq.com>
2019-01-09 21:47:23 +0800

    Modified for python3 support

--- python/draw_net.py
+++ python/draw_net.py
@@ -33,10 +33,6 @@ def parse_args():
                               'TEST, or ALL.  If ALL, then all layers are drawn '
                               'regardless of phase.'),
                         default="ALL")
-    parser.add_argument('--display_lrm', action='store_true',
-                        help=('Use this flag to visualize the learning rate '
-                              'multiplier, when non-zero, for the learning '
-                              'layers (Convolution, Deconvolution, InnerProduct).'))
 
     args = parser.parse_args()
     return args
@@ -55,7 +51,7 @@ def main():
     elif args.phase != "ALL":
         raise ValueError("Unknown phase: " + args.phase)
     caffe.draw.draw_net_to_file(net, args.output_image_file, args.rankdir,
-                                phase, args.display_lrm)
+                                phase)
 
 
 if __name__ == '__main__':
